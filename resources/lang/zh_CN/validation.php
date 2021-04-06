7e3575afb4344932f429e642fb46d7a84b778f43
Kevin181 <yuan364205famile@gmail.com>
2021-04-06 08:14:50 +0000

    中文的错误提示信息

---
+++ resources/lang/zh_CN/validation.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'custom' => [
+        'name' => [
+            'required' => '名称不能为空！',
+        ],
+        'email' => [
+            'required' => '邮箱地址不能为空！',
+        ],
+        'password' => [
+            'required' => '密码不能为空！',
+        ],
+    ],
+];
